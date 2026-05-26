8e87a55144a5841d59190a92b10fd5a082bbbd6e
Talon-Bytes <logperiodicdipolearray@gmail.com>
2026-05-27 03:15:12 +0530

    Update phasor_circle.py
    managed some logical errors

--- phasor_circle.py
+++ phasor_circle.py
@@ -4,31 +4,25 @@
 
 
 import matplotlib.pyplot as plt
-import matplotlib.animation as animat
+import matplotlib.animation as animat 
 import numpy 
-amp = 5 #amplitude
-h = 0
+amp= 5 
 x_component = []
-y_component = []
+y_component= []
 for i in range(0,629):
     h = i/100
     x_component.append(amp*numpy.cos(h))
     y_component.append(amp*numpy.sin(h))
 fig, areaCanvas = plt.subplots()
-areaCanvas.plot(x_component,y_component)#plot points as (x,y) on given canvas (taking it as cartesian plane)
+areaCanvas.plot(x_component,y_component)
 indx=0
-def TheFor(indx):
-    for k,j in zip(x_component, y_component):
-        indx+=1
-        areaCanvas.cla()
-        areaCanvas.plot(x_component,y_component)
-        if indx == (len(x_component)-2):
-            k = x_component[0]
-            j = y_component[0]
-            indx=0
-            TheFor(indx)
-        areaCanvas.quiver(0, 0, k, j, scale=1, scale_units='xy', angles='xy') 
-        plt.pause(0.01)
-        plt.draw()
-TheFor(indx)
+q = areaCanvas.quiver(0, 0, 0, 0, scale=1, scale_units='xy', angles='xy') 
+while True:
+    q.remove()
+    q = areaCanvas.quiver(0, 0, x_component[indx],y_component[indx], scale=1, scale_units='xy', angles='xy')
+    indx+=1
+    plt.pause(0.01)
+    plt.draw()
+    if indx==len(x_component):
+        indx-=indx
 plt.show()
