f0abdaba29708094f84020c5f0fdbc2e9a033004
Talon-Bytes <logperiodicdipolearray@gmail.com>
2026-05-29 01:21:15 +0530

    Update phasor_circle.py
    pressing 'q' key now stops the program and python; pressing the close button on address bar also works now by closing python

--- phasor_circle.py
+++ phasor_circle.py
@@ -6,6 +6,18 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animat 
 import numpy 
+running = True
+fig, areaCanvas = plt.subplots()
+def onClose(event):
+    global running
+    if event.key == 'q' or event.key == 'Q': 
+      running = False
+      plt.close()
+      exit()
+def onX(event):
+    exit()
+fig.canvas.mpl_connect('key_press_event', onClose)
+fig.canvas.mpl_connect('close_event', onX)
 amp= 5 
 x_component = []
 y_component= []
@@ -13,11 +25,10 @@ for i in range(0,629):
     h = i/100
     x_component.append(amp*numpy.cos(h))
     y_component.append(amp*numpy.sin(h))
-fig, areaCanvas = plt.subplots()
 areaCanvas.plot(x_component,y_component)
 indx=0
 q = areaCanvas.quiver(0, 0, 0, 0, scale=1, scale_units='xy', angles='xy') 
-while True:
+while running:
     q.remove()
     q = areaCanvas.quiver(0, 0, x_component[indx],y_component[indx], scale=1, scale_units='xy', angles='xy')
     indx+=1
@@ -25,4 +36,5 @@ while True:
     plt.draw()
     if indx==len(x_component):
         indx-=indx
+
 plt.show()
